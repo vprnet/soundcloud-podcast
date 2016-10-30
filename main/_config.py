82193837e524ba19f63ee4ae17c2e5cec12836e2
748s <nick.wakeman@gmail.com>
2016-10-30 15:52:28 -0400

    Minor tweaks to app to clean it up and make it more reliable with SC's API

--- main/_config.py
+++ main/_config.py
@@ -11,27 +11,30 @@ AWS_BUCKET = 'www.vpr.net'
 AWS_DIRECTORY = 'sandbox/app/'
 
 SOUNDCLOUD_API = {
-	"client_id": "",
-	"client_secret": "",
+    "client_id": "",
+    "client_secret": "",
     "username": "",
-	"password": ""}
-SEARCH_FOR = '' #the track title to search for (SC filter by tag does not work wel)
+    "password": ""
+}
+SEARCH_FOR = '' # the track title to search for (SC filter by tag does not work wel)
 SOUNDCLOUD_META = {
-	"title" : "", #the title of this podcast
-	"description" : "", #podcast description
-	"link" : "", #link to website for this resource (i.e. fairbanksmuseum.org/...)
-	"atom_link": "", #the url for this podcast (e.g. http://www.vpr.net/sandbox/this-podcast/)
-	"webmaster" : "podmaster@vpr.net (VPR PodMaster)",
-	"itunes_author" : "Vermont Public Radio",
-	"itunes_subtitle" : "",
-	"itunes_summary" : "",
-	"itunes_owner" : {
-		"name":"Eye On The Sky",
-		"email" : "podmaster@vpr.net"},
-	"itunes_image" : "", # 1400x1400 image/icon for this podcast - hosted by apps/ or sandbox/dirname/static/images/
-	"itunes_category_1": "", # a valid itunes category
-	"itunes_category_2": "", # a valid itunes subcategory
-	"itunes_keywords": ""}
+    "title" : "", # the title of this podcast
+    "description" : "", # podcast description
+    "link" : "", # link to website for this resource (i.e. fairbanksmuseum.org/...)
+    "atom_link": "", # the url for this podcast (e.g. http://www.vpr.net/sandbox/this-podcast/)
+    "webmaster" : "podmaster@vpr.net (VPR PodMaster)",
+    "itunes_author" : "Vermont Public Radio",
+    "itunes_subtitle" : "",
+    "itunes_summary" : "",
+    "itunes_owner" : {
+        "name":"Eye On The Sky",
+        "email" : "podmaster@vpr.net"
+    },
+    "itunes_image" : "", # 1400x1400 image/icon for this podcast - hosted by apps/ or sandbox/dirname/static/images/
+    "itunes_category_1": "", # a valid itunes category
+    "itunes_category_2": "", # a valid itunes subcategory
+    "itunes_keywords": ""
+}
 
 # Cache Settings (units in seconds)
 STATIC_EXPIRES = 60 * 24 * 3600
@@ -42,8 +45,13 @@ FREEZER_DEFAULT_MIMETYPE = 'application/rss+xml'
 FREEZER_IGNORE_MIMETYPE_WARNINGS = True
 FREEZER_DESTINATION = 'build'
 FREEZER_BASE_URL = 'http://%s/%s' % (AWS_BUCKET, AWS_DIRECTORY)
-FREEZER_STATIC_IGNORE = ['Gruntfile*', 'node_modules', 'package.json',
-    'dev', '.sass-cache']
+FREEZER_STATIC_IGNORE = [
+    'Gruntfile*',
+    'node_modules',
+    'package.json',
+    'dev',
+    '.sass-cache'
+]
 
 WEBFACTION_PATH = AWS_DIRECTORY
 
